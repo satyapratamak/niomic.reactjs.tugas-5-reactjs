668bfbe067d48b03a0efe6ae640d9c40db26a46a
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-01-08 02:34:05 +0700

    COMMIT : Niomic ReactJS tugas-5-reactjs DONE

---
+++ src/Form/FormMakanan.js
@@ -0,0 +1,72 @@
+import React, {Component} from "react";
+
+
+class FormMakanan extends Component{
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            pesanan : "",
+            jumlah : "",
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeJumlah = this.handleChangeJumlah.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.pesanan = React.createRef();
+
+
+    }
+
+    handleChange(e){
+        this.setState({pesanan : e.target.value});
+    }
+
+    handleChangeJumlah(e){
+      this.setState({jumlah : e.target.value});
+    }
+
+    handleSubmit(e){
+      e.preventDefault();
+      alert(
+        "Pesanan Anda Adalah : " + this.state.pesanan + " dengan Jumlah Pesanan :" +this.state.jumlah
+      );
+
+      this.setState({ pesanan:"", jumlah:"",});
+      this.pesanan.current.focus();
+    }
+
+    render(){
+      return(
+        <div>
+          <center>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                value={this.state.pesanan}
+                ref = {this.pesanan}
+                placeholder = "Masukan Pesanan Anda"
+              />
+
+              <input
+                onChange={this.handleChangeJumlah}
+                type="number"
+                value={this.state.jumlah}
+                placeholder = "Masukan Jumlah Pesanan Anda"
+              />
+              <br/>   <br/>
+              <input type="submit" value="Pesan" />
+            </form>
+          </center>
+
+
+        </div>
+      );
+    }
+}
+
+
+export default FormMakanan;
